81bc4603e304693dfafa84824cec05f5aea79b0e
muhammaddariazzidane <dariazzidane@gmail.com>
2022-12-18 20:06:53 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './../components/navbar';
+
+export default function Home() {
+  return (
+    <>
+    <p className="text-red-500 text-7xl " >ini Puan</p>
+    <Navbar />
+    </>
+  )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div className='w-full py-44 bg-slate-300' >
+      <p className='font-bold' >lorem</p>
+    </div>
+  )
+}
